5a7a43acd9a8f456a35de5773be545f6ac8180fe
Your Name <you@example.com>
2023-12-04 14:34:31 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/duydoxuan/learnPublish
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,25 @@
+package debugLearn
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var number int
+
+func loopLearn() {
+	for i := 0; i <= 1000; i++ {
+		if i%100 == 0 {
+			rand.Seed(time.Now().UnixNano())
+			number = rand.Intn(100) // Set number to a random value and limit it for readability
+			fmt.Printf("Index %d, New Number: %d\n", i, number)
+		}
+	}
+	// Calculate the square of the number
+	fmt.Println("The square of the number is:", number*number)
+}
+
+func add(a, b int) int {
+	return a + b
+}
